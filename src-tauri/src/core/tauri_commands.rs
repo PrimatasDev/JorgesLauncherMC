1badecdec99df18cae1d5bb20c8b9dc5f017150a
jv <jvgouvea.br@gmail.com>
2026-05-28 22:43:03 -0300

    Correções de comentários

--- src-tauri/src/core/tauri_commands.rs
+++ src-tauri/src/core/tauri_commands.rs
@@ -1,9 +1,8 @@
 //# ───▶ Importações do JorgesLauncherMC
 use super::{LaunhcerOptions, get_instance_dir, load_configs, save_configs};
 
-//& ───▶ Abrir diretório ao APP ◀──────────────────────────────────────
-//$ Abrir diretório "INSTANCE" do aplicativo no gerenciador de arquivos do sistema
 #[tauri::command]
+//$ Abrir diretório "INSTANCE" do aplicativo no gerenciador de arquivos do sistema
 pub fn open_instance_dir() -> Result<(), String> {
     //% Criar pasta "INSTANCE" dentro da pasta raiz do projeto
     let instance_dir = get_instance_dir().map_err(|e| e.to_string())?;
@@ -15,17 +14,15 @@ pub fn open_instance_dir() -> Result<(), String> {
     Ok(())
 }
 
-//& ───▶ Salvar configurações ◀──────────────────────────────────────
-//$ Salvar configurações do tipo "LaunhcerOptions" em um arquivo CFG_JSON na pasta raiz do app
 #[tauri::command]
+//$ Salvar configurações do tipo "LaunhcerOptions" em um arquivo CFG_JSON na pasta raiz do app
 pub fn save_launcher_configs(options: LaunhcerOptions) -> Result<(), String> {
     save_configs(&options).map_err(|e| format!("Falha ao salvar o arquivo. \n ERRO: {}", e))?;
     Ok(())
 }
 
-//& ───▶ Carregar configurações ◀──────────────────────────────────────
-//$ Carregar configurações do arquivo CFG_JSON na pasta raiz do app
 #[tauri::command]
+//$ Carregar configurações do arquivo CFG_JSON na pasta raiz do app
 pub fn load_launcher_configs() -> Result<LaunhcerOptions, String> {
     let options =
         load_configs().map_err(|e| format!("Falha ao carregar o arquivo. \n ERRO: {}", e))?;
